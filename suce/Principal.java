be3f92eeeb06c7fc09304ee2f090b7e016791e7b
Sucena <gssk2007@gmail.com>
2026-03-02 15:00:02 -0300

    Update Principal.java

--- suce/Principal.java
+++ suce/Principal.java
@@ -1,1 +1,20 @@
-public class Principal{}
+public class Principal{
+
+public static void main(String args[]){
+
+  Produto produto1,produto2;
+
+  produto1= new Produto();
+  produto2= new Produto();
+
+  produto1.setNome("Bala");
+  produto2.setNome("Doce";
+  produto1.setQuantidade(100);
+  produto2.setQuantidade(500);
+  produto1.setPreco(1.50);
+  produto2.setPreco(3.00);
+
+  
+}
+  
+}
